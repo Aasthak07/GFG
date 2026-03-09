b7fc06e1dddb2e6b3fa2eba940ae9c41c76a6b48
Aastha Kumari <aasthakumari20002@gmail.com>
2026-03-10 01:36:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First and Second Smallests/first-and-second-smallests.py
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.py
@@ -1,30 +1,19 @@
-# class Solution:
-#     def minAnd2ndMin(self, arr):
-#         if len(arr) < 2:
-#             return -1
-        
-#         arr = sorted(set(arr))
-        
-#         if len(arr) < 2:
-#             return -1
-        
-#         return arr[0], arr[1]
 class Solution:
     def minAnd2ndMin(self, arr):
-        if len(arr) < 2:
-            return [-1]
+        # code here
+        n= len(arr)
+        smallest = second_smallest= float('inf')
         
-        smallest = float('inf')
-        second_smallest = float('inf')
         
-        for num in arr:
-            if num < smallest:
-                second_smallest = smallest
-                smallest = num
-            elif smallest < num < second_smallest:
-                second_smallest = num
         
+        for i in range(n):
+            if arr[i]< smallest:
+                second_smallest= smallest
+                smallest = arr[i]
+                
+            elif arr[i] > smallest and arr[i] < second_smallest:
+                second_smallest = arr[i]
+                
         if second_smallest == float('inf'):
-            return [-1]
-        
+            return [-1]        
         return [smallest, second_smallest]

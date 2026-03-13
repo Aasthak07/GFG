d71773a911e99869fbecb0567c996e3b043d64de
Aastha Kumari <aasthakumari20002@gmail.com>
2026-03-14 00:38:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Duplicates from unsorted array/remove-duplicates-from-unsorted-array.py
@@ -0,0 +1,15 @@
+#User function Template for python3
+
+class Solution:
+    def removeDuplicate(self, arr):
+        seen = set()
+        res = []
+        
+        for num in arr:
+            if num not in seen:
+                seen.add(num)
+                res.append(num)
+                
+        return res
+    
+
